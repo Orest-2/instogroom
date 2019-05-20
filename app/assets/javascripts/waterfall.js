4893be20423e2813dab7899d591ce48273e18111
Reidond <reidond@gmail.com>
2019-05-20 21:13:39 +0300

    Add watterfall

--- app/assets/javascripts/waterfall.js
+++ app/assets/javascripts/waterfall.js
@@ -1,8 +1,34 @@
+/* eslint-disable no-unused-vars */
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js";
+import {
+	BASE_URL
+} from "/js/config.js";
+import {
+	store
+} from "/js/store/store.js";
+import {
+	HttpService
+} from "/js/services/HttpService.js";
+import {
+	getAllProfiles
+} from "/js/common.js";
+
+const http = new HttpService(BASE_URL);
 
 let app = new Vue({
-	el: "#waterfall",
+	el: "#watterfall",
 	data: {
-		message: "Hello Waterfall!"
+		store: store,
+	},
+	methods: {
+		follow(userId) {
+			http.post('/explores/unfollow', {
+				followed_id: userId
+			}).then(data => {
+				getAllProfiles();
+			})
+		}
 	}
 });
+
+getAllProfiles();
