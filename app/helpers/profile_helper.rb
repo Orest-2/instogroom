61e8b4f5c458b1ae52fcce9c3ebcb4405e7aa8f2
Orest <orest-2@yandex.ua>
2019-05-21 20:19:41 +0300

    Fix

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -6,8 +6,8 @@ module ProfileHelper
 			avatar: avatar.attachment.nil? ? "" : url_for(avatar),
 			instopics: get_intopics(id),
 			followed: !current_user.follows.find_by(followed_id: id).nil?,
-			followersCount: current_user.follows.length,
-			followedCount: Follow.where(followed_id: current_user.id).length
+			followedCount: current_user.follows.length,
+			followersCount: Follow.where(followed_id: current_user.id).length
 		})
 	end
 end
