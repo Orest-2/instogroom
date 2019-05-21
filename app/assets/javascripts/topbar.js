8f1abd556151fb1dbb2c616cd431855f95675caa
Orest <orest-2@yandex.ua>
2019-05-21 13:36:40 +0300

    Add filepond for image uploading

--- app/assets/javascripts/topbar.js
+++ app/assets/javascripts/topbar.js
@@ -1,4 +1,4 @@
-import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js";
+// import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js";
 import { BASE_URL } from "/js/config.js";
 import { store } from "/js/store/store.js";
 import { HttpService } from "/js/services/HttpService.js";
@@ -7,17 +7,30 @@ const http = new HttpService(BASE_URL);
 
 let app = new Vue({
 	el: "#topbar",
-	data: {
-		store: store,
-		formInstopic: {
-			picture: "",
-			description: ""
-		}
+	data: function() {
+		return {
+			store: store,
+			formInstopic: {
+				picture: "",
+				description: ""
+			}
+		};
+	},
+	components: {
+		FilePond: vueFilePond.default(
+			FilePondPluginImagePreview,
+			FilePondPluginFileValidateSize
+		)
 	},
 	methods: {
-		
-		selectedInstopic(event) {
-			let file = event.target.files[0];
+		submitAddInstopic() {
+			http
+				.post("/instopic/create", this.formInstopic)
+				.then(data => (store.user.instopics = data));
+		},
+		test() {
+			let file = this.$refs.pond.getFiles()[0].file;
+
 			var fileSize = file.size / 1024 / 1024;
 			var reader = new FileReader();
 
@@ -31,14 +44,6 @@ let app = new Vue({
 			} else {
 				alert("File is too big! Size > 5MB");
 			}
-		},
-		
-		submitAddInstopic() {
-			http
-				.post("/instopic/create", this.formInstopic)
-				.then(data => (store.user.instopics = data));
 		}
 	}
 });
-
-
