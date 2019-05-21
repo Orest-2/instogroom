ad929b05cd8f801a5316cefb1436c57c7fbbcd97
Orest <orest-2@yandex.ua>
2019-05-21 16:17:07 +0300

    Add profile header block

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-unused-vars */
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js";
-import {
-	BASE_URL
-} from "/js/config.js";
-import {
-	store
-} from "/js/store/store.js";
-import {
-	HttpService
-} from "/js/services/HttpService.js";
+import { BASE_URL } from "/js/config.js";
+import { store } from "/js/store/store.js";
+import { HttpService } from "/js/services/HttpService.js";
 
 const http = new HttpService(BASE_URL);
 
@@ -21,10 +15,6 @@ let app = new Vue({
 			name: "",
 			age: 0,
 			sex: ""
-		},
-		formInstopic: {
-			picture: "",
-			description: ""
 		}
 	},
 	methods: {
@@ -44,31 +34,10 @@ let app = new Vue({
 				alert("File is too big! Size > 10MB");
 			}
 		},
-		selectedInstopic(event) {
-			let file = event.target.files[0];
-			var fileSize = file.size / 1024 / 1024;
-			var reader = new FileReader();
-
-			if (fileSize <= 10) {
-				reader.readAsDataURL(file);
-				reader.onload = () =>
-					(this.formInstopic.picture = {
-						data: reader.result,
-						filename: file.name
-					});
-			} else {
-				alert("File is too big! Size > 5MB");
-			}
-		},
 		submitEditUser() {
 			http
 				.put("/profile/update", this.formUser)
 				.then(data => (store.user = data));
-		},
-		submitAddInstopic() {
-			http
-				.post("/instopic/create", this.formInstopic)
-				.then(data => (store.user.instopics = data));
 		}
 	}
 });
