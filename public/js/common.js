ecf4eec7a10d35e2e37ada819aad45c6ca01bfa4
Orest <orest-2@yandex.ua>
2019-05-21 20:10:02 +0300

    Fix login wrong page render

--- public/js/common.js
+++ public/js/common.js
@@ -1,19 +1,15 @@
-import {
-	store
-} from './store/store.js'
-import {
-	HttpService
-} from './services/HttpService.js'
-import {
-	BASE_URL
-} from "./config.js"
+import { store } from "./store/store.js";
+import { HttpService } from "./services/HttpService.js";
+import { BASE_URL } from "./config.js";
 
 const http = new HttpService(BASE_URL);
 
 export function getAllProfiles(isWatterfall) {
 	let temp = [];
-	http.get(`/explores/all_profiles`).then(data => {
-		let onlyUnfollowers = data.filter((element) => element.followed === isWatterfall)
+	http.post(`/explores/all_profiles`).then(data => {
+		let onlyUnfollowers = data.filter(
+			element => element.followed === isWatterfall
+		);
 		onlyUnfollowers.forEach(unfollowers => {
 			unfollowers.instopics.forEach(instopic => {
 				instopic.name = unfollowers.name;
